51963e8304a903c8dd69949954c1776186a83b0d
Rabbi hossain <rh7391679@gmail.com>
2024-07-03 20:14:02 -0700

    first commit

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 const express = require('express');
 const cors = require('cors');
+var jwt = require('jsonwebtoken');
+var cookieParser = require('cookie-parser')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser())
 
 
 console.log(process.env.DB_PASS)
+console.log(process.env.DB_USER)
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dqtrbnk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -23,6 +31,32 @@ const client = new MongoClient(uri, {
     }
 });
 
+// middleware
+
+const logger = (req, res, next)=>{
+    console.log('log :info', req.method, req.url)
+    next()
+}
+
+const verifyToken = (req, res, next)=>{
+    const token = req?.cookies?.token
+    // console.log('token in the middleware', token)
+    if(!token){
+        return req.status(401).send({message: 'unauthorized access'})
+    }
+
+    jwt.verify(token, process.env.DB_TOKEN_SECRET, (err, decoded)=>{
+        
+        if(err){
+            return res.status(401).send({message: 'unauthorized access'})
+        }
+        req.user = decoded
+        next()
+    })
+
+    // next()
+}
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
@@ -31,6 +65,29 @@ async function run() {
         const serviceCollection = client.db('carDoctor').collection('services');
         const bookingCollection = client.db('carDoctor').collection('bookings');
 
+        // auth related api
+        app.post('/jwt', logger,  async(req, res)=>{
+            const user = req.body
+            console.log(user)
+            const token = jwt.sign(user, process.env.DB_TOKEN_SECRET, {expiresIn: '1h'})
+
+            res.cookie('token', token, {
+                httpOnly: true,
+                secure: true,
+                sameSite: 'none'
+            })
+            .send({success: true})
+        })
+
+        app.post('/logout', async(req, res)=>{
+            const user = req.body
+            console.log('login out',user)
+            res.clearCookie('token', {maxAge: 0})
+            .send({success: true})
+        })
+
+
+        // services related api
         app.get('/services', async (req, res) => {
             const cursor = serviceCollection.find();
             const result = await cursor.toArray();
@@ -52,8 +109,12 @@ async function run() {
 
 
         // bookings 
-        app.get('/bookings', async (req, res) => {
+        app.get('/bookings', logger,verifyToken, async (req, res) => {
             console.log(req.query.email);
+            console.log('token owner info', req.user)
+            if(req.user.email !== req.query.email){
+                return res.status(403).send({message: 'forbiden access'})
+            }
             let query = {};
             if (req.query?.email) {
                 query = { email: req.query.email }
